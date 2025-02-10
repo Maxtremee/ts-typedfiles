e4ceb4a3ee12cded93438b1da6d616550cec6602
Maxtremee <maksymilian.j.zadka@gmail.com>
2025-02-10 20:57:38 +0100

    chore: remove test file from gitignore

---
+++ src/test.d.ts
@@ -0,0 +1,8 @@
+// THIS FILE IS GENERATED AUTOMATICALLY. DO NOT EDIT.
+export type FilePath =
+  | "test-dir/.hidden"
+  | "test-dir/1/2/3/.hidden3"
+  | "test-dir/1/2/visible2.extension"
+  | "test-dir/1/visible1.extension"
+  | "test-dir/visible"
+  | "test-dir/visible.extension"
