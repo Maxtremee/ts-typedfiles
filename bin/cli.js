0493f74c70dee7bbcc9cadd992da3f1334f5b87a
Maksymilian Zadka <maksymilian.j.zadka@gmail.com>
2025-03-31 10:46:02 +0200

    feat: bin

---
+++ bin/cli.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import { defineCommand, runMain } from "citty"
+import { generateFilePathsType } from "../dist/index.js";
+
+const main = defineCommand({
+  meta: {
+    name: "ts-typedfiles",
+    description: "Generate filepaths from directory structure",
+  },
+  args: {
+    path: {
+      type: "positional",
+      description: "The path to the directory to scan",
+      required: true,
+    },
+    output: {
+      type: "positional",
+      description: "The path to the output TypeScript file",
+      default: "./src/types/file-paths.d.ts",
+      required: false,
+    },
+    typeName: {
+      type: "string",
+      description: "The name of the type to be generated",
+      default: "FilePath",
+      required: false,
+    },
+  },
+  run({ args }) {
+    generateFilePathsType(args.path, args.output, args.typeName)
+  },
+})
+
+runMain(main)
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,3 @@
-import { defineCommand, runMain } from "citty"
 import { generateFilePathsType } from "./parser.js"
-
-const main = defineCommand({
-  meta: {
-    name: "ts-typedfiles",
-    description: "Generate filepaths from directory structure",
-  },
-  args: {
-    path: {
-      type: "positional",
-      description: "The path to the directory to scan",
-      required: true,
-    },
-    output: {
-      type: "positional",
-      description: "The path to the output TypeScript file",
-      default: "./src/types/file-paths.d.ts",
-      required: false,
-    },
-    typeName: {
-      type: "string",
-      description: "The name of the type to be generated",
-      default: "FilePath",
-      required: false,
-    },
-  },
-  run({ args }) {
-    generateFilePathsType(args.path, args.output, args.typeName)
-  },
-})
-
-runMain(main)
 
 export { generateFilePathsType }
